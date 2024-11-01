7612d8418f8f537ad262c23271bf0c90109f6031
Ursula Ainso <ursulaainso@gmail.com>
2024-11-01 10:19:05 +0200

    Shopping list

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,67 @@
-<script></script>
+<script>
+    let newItem = '';
+	
+    let todoList = [{text: 'Potatoes', status: true},
+                    {text: 'Pears', status: false},
+                    {text: 'Milk', status: false},
+                    {text: 'Cheese', status: false},
+                    {text: 'Bananas', status: false}];
+	
+	function addToList() {
+		todoList = [...todoList, {text: newItem, status: false}];
+		newItem = '';
+	}
+	
+	function removeFromList(index) {
+		todoList.splice(index, 1)
+		todoList = todoList;
+    }
+</script>
 
-<h1>Welcome to SvelteKit</h1>
-<input type="text">
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<main>
+
+<h1>My shopping List</h1>
+    
+<input id="add" bind:value={newItem} type="text" placeholder="Add to shopping list">
+<button on:click={addToList}>Add</button>
+
+<br/>
+{#each todoList as item, index}
+	<input bind:checked={item.status} type="checkbox">
+	<span class:checked={item.status}>{item.text}</span>
+	
+	<br/>
+{/each} 
+
+</main>
+
+<style> 
+	.checked {
+        text-decoration: line-through;
+    }
+
+    span {
+        margin: 5px 0px;
+    }
+
+    button {
+        background-color: rgb(91, 196, 133);
+        border: 1px grey;
+        padding: 7px 16px;
+        border-radius: 4px;
+    }
+
+    main {
+        margin-left: 20px;
+        font-family: 'Trebuchet MS', sans-serif;
+    }
+
+    #add {
+        margin-bottom: 20px;
+        padding: 6px 8px;
+        height: 18px;
+        background-color: rgb(228, 228, 228);
+        border: 0px;
+        border-radius: 4px;
+    }
+</style> 
